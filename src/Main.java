8e31d2ca403180f2b51084e9665a001b88f88cf1
ofcmail <ofcmailkom@gmail.com>
2022-12-02 15:00:32 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        int number = 10;
-        double FirstNumberToCompare = 9.45;
-        double SecondNumberToCompare = 8.5;
-        boolean IsSecondNumberCloser = (SecondNumberToCompare > FirstNumberToCompare && SecondNumberToCompare < number && FirstNumberToCompare < number);
-        boolean IsFirstNumberCloser = (FirstNumberToCompare > SecondNumberToCompare && FirstNumberToCompare < number && SecondNumberToCompare < number);
-        if (IsFirstNumberCloser) {
-            System.out.println("Число " + FirstNumberToCompare + " ближе к 10 по значению.");
-        } else if (IsSecondNumberCloser) {
-            System.out.println("Число " + SecondNumberToCompare + " ближе к 10 по значению.");
+        int number = 5;
+        double firstNumberToCompare = 9.45;
+        double secondNumberToCompare = 8.5;
+        boolean isSecondNumberCloser = (secondNumberToCompare > firstNumberToCompare && secondNumberToCompare < number && firstNumberToCompare < number);
+        boolean isFirstNumberCloser = (firstNumberToCompare > secondNumberToCompare && firstNumberToCompare < number && secondNumberToCompare < number);
+        if (isFirstNumberCloser) {
+            System.out.println("Число " + firstNumberToCompare + " ближе к " + number + " по значению.");
+        } else if (isSecondNumberCloser) {
+            System.out.println("Число " + secondNumberToCompare + " ближе к " + number + " по значению.");
         } else {
             System.out.println("Некорректное условие. Проверяемые числа не должны быть равны друг другу, заданному числу или превышать значение заданного числа. " +
                     "Задайте другие значения.");
